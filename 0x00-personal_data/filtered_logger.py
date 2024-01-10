396e0c4ea38343e460dd94b7188d17b3be6b1e70
MercyNJ <mercinjuguna@gmail.com>
2024-01-10 09:59:33 +0000

    Creating a logger for user data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -49,3 +52,22 @@ def filter_datum(fields: List[str], redaction: str,
     return re.sub(
             r'(\b(?:{}))[^{};]+'.format("|".join(fields), re.escape(
                 separator)), r'\1=' + redaction, message, flags=re.MULTILINE)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Create and configure the 'user_data' logger.
+    """
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=list(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    logger.propagate = False
+
+    return logger
