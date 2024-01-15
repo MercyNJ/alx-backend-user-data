714f07ae1e0e54f960604a29f4df2d8215a6a246
MercyNJ <mercinjuguna@gmail.com>
2024-01-15 18:21:47 +0000

    Filtering requests to determine which needs autentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves the Authorization header from the request """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Retrieves the current user from the request """
