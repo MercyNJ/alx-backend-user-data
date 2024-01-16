19721d3e6ad794599a2c6e76452581caad60388d
MercyNJ <mercinjuguna@gmail.com>
2024-01-16 09:12:07 +0000

    Method that returns the User instance based on credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic authentication module.
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -57,3 +58,23 @@ class BasicAuth(Auth):
         if len(credentials) != 2:
             return (None, None)
         return tuple(credentials)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            available_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in available_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
