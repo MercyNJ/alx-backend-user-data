03a5dfae0c0325fcc01c36a162e52754aeb92f75
MercyNJ <mercinjuguna@gmail.com>
2024-01-16 09:57:26 +0000

    use maxsplit param to allow passwords with colon

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,7 +56,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
 
-        credentials = decoded_base64_authorization_header.split(':')
+        credentials = decoded_base64_authorization_header.split(':', 1)
         if len(credentials) != 2:
             return (None, None)
         return tuple(credentials)
