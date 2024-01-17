b33fea256ead0b40322fd8063e02695243255267
MercyNJ <mercinjuguna@gmail.com>
2024-01-17 12:33:39 +0000

    A view that handles all routes for the Session authentication

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Module of Session authentication views
+"""
+
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login():
+    """
+    Handle the authentication process for Session ID.
+    """
+    from api.v1.app import auth
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        users = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not users or users == []:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in users:
+        if user.is_valid_password(password):
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            response.set_cookie(session_name, session_id)
+            return response
+    return jsonify({"error": "wrong password"}), 401
