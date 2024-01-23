f3011859a296da36a526f739edec4b54491cdc73
MercyNJ <mercinjuguna@gmail.com>
2024-01-23 08:28:55 +0000

    Find user method added

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,4 +39,17 @@ class DB:
         self._session.add(user)
         self._session.commit()
 
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user in the db based on given args.
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound
         return user
